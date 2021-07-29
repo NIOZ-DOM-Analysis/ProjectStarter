e4ea222cc963cfc75b990f7a21ee20d3c8cbc9ec
MilouArts <70600687+MilouArts@users.noreply.github.com>
2021-07-29 16:31:40 +0200

    Update create_directory.R
    Changed 'adventure' to 'Project'

--- Scripts/create_directory.R
+++ Scripts/create_directory.R
@@ -14,25 +14,24 @@ List of alterations:
 ifelse(!dir.exists(file.path(main.data.path, Owner.initials)), dir.create(file.path(main.data.path, Owner.initials)), FALSE)
 
 # add owner initals to string
-data.path <- paste0(main.data.path, '\\', Owner.initials)
+data.path <- file.path(main.data.path, Owner.initials)
 
-# Adventure year. check if directory exist, otherwise create new
-ifelse(!dir.exists(file.path(data.path, Adventure.year)), dir.create(file.path(data.path, Adventure.year)), FALSE)
+# Project year. check if directory exist, otherwise create new
+ifelse(!dir.exists(file.path(data.path, Project.year)), dir.create(file.path(data.path, Project.year)), FALSE)
 
-# add Adventure year to string for data path
-data.path <- paste0(data.path, '\\', Adventure.year) 
+# add Project year to string for data path
+data.path <- file.path(data.path, Project.year) 
 
-# Adventure name. check if folder exist, otherwise create new
-ifelse(!dir.exists(file.path(data.path, Adventure.name)), dir.create(file.path(data.path, Adventure.name)), FALSE)
+# Project name. check if folder exist, otherwise create new
+ifelse(!dir.exists(file.path(data.path, Project.name)), dir.create(file.path(data.path, Project.name)), FALSE)
 
-# add Adventure.name to string for data path
-data.path <- paste0(data.path, '\\', Adventure.name)
+# add Project.name to string for data path
+data.path <- file.path(data.path, Project.name)
 
 # Experiment name. check if directory to safe files exist, otherwise create new
+if (exists(sub.experiment.name)){
 ifelse(!dir.exists(file.path(data.path, Experiment.name)), dir.create(file.path(data.path, Experiment.name)), FALSE)
 
 # add Experiment name to string for data path
-data.path <- paste0(data.path, '\\', Experiment.name)
-
-# within this folder we are creating sub folders.
-# check if  the direction already exists, otherwise create new
+data.path <- file.path(data.path, Experiment.name)
+}
