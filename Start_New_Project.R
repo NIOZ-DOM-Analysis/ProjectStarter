54d18eb1250e486c8140eca77f29f4189c6bd40a
Lisa Schellenberg <69227137+lisas0704@users.noreply.github.com>
2021-08-05 08:53:51 +0200

    Update Start_New_Project.R

--- Start_New_Project.R
+++ Start_New_Project.R
@@ -54,7 +54,7 @@ orbitrapsequence.csv (File Name and Sample Name from orbitrap sequence + added c
 SIRIUS.mgf (exportfile from SIRIUS -> not from MZMINE! UNZIP the GNPS SIRIUS file in raw data folder)
 metadata.csv (metadata file)
 
-download the cytoscape data from GNPS and unzip into the Cytoscape folder and name the folder `analogs_on` and make sure there is a list of analogs
+download the cytoscape data from GNPS and unzip into the previously created Cytoscape folder named `Analogs_on` and make sure there is a list of analogs
 
 ##not yet done, but a good one also make a main_project_info_XXX.R in the R folder with the info of the experiment so that you dont have to change it all the time when
 you analyzed a different project and therefore changed the main script.
